881c8a7ee75ad9163c3cb8078f0f53569d6f11f3
kunao_learner <15346777800@163.com>
2024-09-29 22:36:08 +0800

    Add files via upload

---
+++ start3.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, T, F, M, N, Q;
+	cin >> a >> b;
+	T = a + b;
+	F = a - b;
+	M = a * b;
+	N = a / b;
+	Q = a % b;
+
+	cout << a << "+" << a << "=" << T << endl;
+	cout << a << "-" << a << "=" << F << endl;
+	cout << a << "*" << a << "=" << M << endl;
+	cout << a << "/" << a << "=" << N << "-" << "-" << "-" << Q << endl;
+
+
+	return 0;
+}
