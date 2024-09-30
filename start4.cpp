fb6f590d73d5232b47c5f7c8ae377792cdc88e9d
kunao_learner <15346777800@163.com>
2024-09-30 23:26:06 +0800

    Add files via upload

---
+++ start4.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{   
+	int a, b, c;
+	cout << "输入a,b进行加法运算；" << endl;
+	cout << "输入a:";
+	cin >> a;
+	cout << "输入b:";
+	cin >> b;
+	c = a + b;
+	cout << a << "+" << b << "=" << c << endl;
+	cout << "谢谢使用！";
+
+	return 0;
+}
