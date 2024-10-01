0018ebc9335f0c52917c3e8eb31313e2d812e33e
kunao_learner <15346777800@163.com>
2024-10-01 22:40:48 +0800

    Add files via upload

---
+++ start6.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cout << "今天是几月：";
+	cin >> a;
+	cout << "今天是几日：";
+	cin >> b;
+	cout << endl << endl;
+	c = a + b;
+
+	cout << "今天是10月1日！" << endl << "是国庆节！" << endl << "是祖国母亲的生日！" << endl << "祝中国母亲生日快乐，永远繁荣昌盛！！！" << endl;
+
+	return 0;
+}
