3038b103580dea0c6a8b1447d4988c7e86184d9a
kunao_learner <15346777800@163.com>
2024-10-01 22:16:29 +0800

    Add files via upload

---
+++ start5.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{   
+	int a, b, T, F, N, M, Q;
+	cout << "欢迎使用a,b加减乘除计算机：" << endl << endl;
+	cout << "请输入a=";
+		cin >> a;
+	cout << "请输入b=";
+	    cin >> b;
+
+    T = a + b;
+	F = a - b;
+	N = a * b;
+	M = a / b;
+	Q = a % b;
+
+	cout << a << "+" << b << "=" << T << endl;
+	cout << a << "-" << b << "=" << F << endl;
+	cout << a << "*" << b << "=" << N << endl;
+	cout << a << "/" << b << "=" << M << "---" << Q << endl << endl;
+
+	cout << "谢谢使用！" << endl;
+
+	return 0;
+}
