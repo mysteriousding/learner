33a6e401d8334e267595fde2839e60859bdee5c4
kunao_learner <15346777800@163.com>
2024-10-02 22:54:05 +0800

    Add files via upload

---
+++ max2.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+using namespace std;
+int max(int x, int y,int z)
+{
+	int k;
+	if (x > y) k = x;
+	   else k = y;
+	if (k > z)k = k;
+	   else k = z;
+
+	return (k);
+}
+
+int main()
+{
+	int a, b, c, m;
+	cin >> a >> b >> c;
+	m = max(a, b, c);
+	cout << "max=" << m << endl;
+
+	return 0;
+}
