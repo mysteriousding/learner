75907401119c66defd37eeaa13c99ee2449b15f2
kunao_learner <15346777800@163.com>
2024-10-04 23:19:52 +0800

    Add files via upload

---
+++ class2.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+using namespace std;
+class Student
+{
+private:
+	int num;
+	int score;
+public:
+	void setdata()
+	{
+		cin >> num;
+		cin >> score;
+	}
+	void display()
+	{
+		cout << "学号：" << num << endl;
+		cout << "成绩：" << score << endl;
+		cout << "-------------" << endl;
+	};
+};
+Student stud1, stud2;
+int main()
+{
+	stud1.setdata();
+	stud2.setdata();
+	stud1.display();
+	stud2.display();
+
+	return 0;
+}
