ec380ad382fa424158c343a3cc313f8195bd0e15
kunao_learner <15346777800@163.com>
2024-10-05 23:23:36 +0800

    Add files via upload

---
+++ exercises3.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+    //错误：int a,b;                      
+    //      c=a+b;
+    //      cout>>"a+b=">>a+b;
+
+    //改正：(共四处错误）
+    int a, b, c;
+    cin >> a >> b;
+    c = a + b;
+    cout << "a+b=" << c << endl;
+
+    return 0;
+}
