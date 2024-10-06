8cc06198c4033efbdb72c4f4b7a4b20cd1e6eb32
kunao_learner <15346777800@163.com>
2024-10-06 20:53:05 +0800

    Add files via upload

---
+++ exercises5.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+using namespace std;
+//错误：
+//int main();
+//{
+//   int a,b;
+//   c=add(a,b)
+//   cout<<"a+b="<<c<<endl;
+//   return 0;
+//}
+//int add(int x,int y);
+//{
+//   z=x+y;
+//   return(z）；
+// }
+//改正：                 （共五处错误）
+int main()
+{
+	int add(int x, int y);
+	int a, b, c;
+	cin >> a >> b;
+	c = add(a, b);
+	cout << "a+b=" << c << endl;
+
+	return 0;
+}
+int add(int x, int y)
+{
+	int z;
+	z = x + y;
+
+	return(z);
+}
