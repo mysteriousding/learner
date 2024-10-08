cdc6a6b2c80e856ad1e9055b64e6250d2b7c224c
kunao_learner <15346777800@163.com>
2024-10-08 13:45:13 +0800

    Add files via upload

---
+++ med1.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int med(int x, int y, int z);
+	int a, b, c, m;
+	cin >> a >> b >> c;
+	m = med(a, b, c);
+	cout << "med=" << m << endl;
+
+	return 0;
+}
+int med(int x, int y, int z)
+{
+	int m0, m1, m2, m3;
+	m1 = x;
+	m2 = y;
+	m3 = z;
+
+	if (m1 > m2) { m1 = m1; m2 = m2; }
+	else { m0 = m1; m1 = m2; m2 = m0; }
+	if (m2 > m3) { m2 = m2; m3 = m3; }
+	else { m0 = m2; m2 = m3; m3 = m0; }
+	if (m1 > m2) { m1 = m1; m2 = m2; }
+	else { m0 = m1; m1 = m2; m2 = m0; }
+	if (m2 > m3) { m2 = m2; m3 = m3; }
+	else { m0 = m2; m2 = m3; m3 = m0; }
+	if (m1 > m2) { m1 = m1; m2 = m2; }
+	else { m0 = m1; m1 = m2; m2 = m0; }
+	if (m2 > m3) { m2 = m2; m3 = m3; }
+	else { m0 = m2; m2 = m3; m3 = m0; }
+
+	return(m2);
+}
