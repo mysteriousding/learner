c25be6b657404da1f097450859275ec9fa46e452
kunao_learner <15346777800@163.com>
2024-10-08 17:00:27 +0800

    Update med3.cpp

--- med3.cpp
+++ med3.cpp
@@ -28,157 +28,157 @@ int med(int a, int b, int c, int d, int e, int f, int g, int h, int i)
 	
 
 	if (m1 > m2) { m1 = m1; m2 = m2; }
-	else { m0 = m1; m1 = m2; m2 = m0; }
+	   else { m0 = m1; m1 = m2; m2 = m0; }
 	if (m2 > m3) { m2 = m2; m3 = m3; }
-	else { m0 = m2; m2 = m3; m3 = m0; }
+	   else { m0 = m2; m2 = m3; m3 = m0; }
 	if (m3 > m4) { m3 = m3; m4 = m4; }
-	else { m0 = m3; m3 = m4; m4 = m0; }
+	   else { m0 = m3; m3 = m4; m4 = m0; }
 	if (m4 > m5) { m4 = m4; m5 = m5; }
-	else { m0 = m4; m4 = m5; m5 = m0; }
+	   else { m0 = m4; m4 = m5; m5 = m0; }
 	if (m5 > m6) { m5 = m5; m6 = m6; }
-	else { m0 = m5; m5 = m6; m6 = m0; }
+	   else { m0 = m5; m5 = m6; m6 = m0; }
 	if (m6 > m7) { m6 = m6; m7 = m7; }
-	else { m0 = m6; m6 = m7; m7 = m0; }
+	   else { m0 = m6; m6 = m7; m7 = m0; }
 	if (m7 > m8) { m7 = m7; m8 = m8; }
-	else { m0 = m7; m7 = m8; m8 = m0; }
+	   else { m0 = m7; m7 = m8; m8 = m0; }
 	if (m8 > m9) { m8 = m8; m9 = m9; }
-	else { m0 = m8; m8 = m9; m9 = m0; }
+	   else { m0 = m8; m8 = m9; m9 = m0; }
 
 	if (m1 > m2) { m1 = m1; m2 = m2; }
-	else { m0 = m1; m1 = m2; m2 = m0; }
+	   else { m0 = m1; m1 = m2; m2 = m0; }
 	if (m2 > m3) { m2 = m2; m3 = m3; }
-	else { m0 = m2; m2 = m3; m3 = m0; }
+	   else { m0 = m2; m2 = m3; m3 = m0; }
 	if (m3 > m4) { m3 = m3; m4 = m4; }
-	else { m0 = m3; m3 = m4; m4 = m0; }
+	   else { m0 = m3; m3 = m4; m4 = m0; }
 	if (m4 > m5) { m4 = m4; m5 = m5; }
-	else { m0 = m4; m4 = m5; m5 = m0; }
+	   else { m0 = m4; m4 = m5; m5 = m0; }
 	if (m5 > m6) { m5 = m5; m6 = m6; }
-	else { m0 = m5; m5 = m6; m6 = m0; }
+	   else { m0 = m5; m5 = m6; m6 = m0; }
 	if (m6 > m7) { m6 = m6; m7 = m7; }
-	else { m0 = m6; m6 = m7; m7 = m0; }
+	   else { m0 = m6; m6 = m7; m7 = m0; }
 	if (m7 > m8) { m7 = m7; m8 = m8; }
-	else { m0 = m7; m7 = m8; m8 = m0; }
+	   else { m0 = m7; m7 = m8; m8 = m0; }
 	if (m8 > m9) { m8 = m8; m9 = m9; }
-	else { m0 = m8; m8 = m9; m9 = m0; }
+	   else { m0 = m8; m8 = m9; m9 = m0; }
 
 	if (m1 > m2) { m1 = m1; m2 = m2; }
-	else { m0 = m1; m1 = m2; m2 = m0; }
+	   else { m0 = m1; m1 = m2; m2 = m0; }
 	if (m2 > m3) { m2 = m2; m3 = m3; }
-	else { m0 = m2; m2 = m3; m3 = m0; }
+	   else { m0 = m2; m2 = m3; m3 = m0; }
 	if (m3 > m4) { m3 = m3; m4 = m4; }
-	else { m0 = m3; m3 = m4; m4 = m0; }
+	   else { m0 = m3; m3 = m4; m4 = m0; }
 	if (m4 > m5) { m4 = m4; m5 = m5; }
-	else { m0 = m4; m4 = m5; m5 = m0; }
+	   else { m0 = m4; m4 = m5; m5 = m0; }
 	if (m5 > m6) { m5 = m5; m6 = m6; }
-	else { m0 = m5; m5 = m6; m6 = m0; }
+	   else { m0 = m5; m5 = m6; m6 = m0; }
 	if (m6 > m7) { m6 = m6; m7 = m7; }
-	else { m0 = m6; m6 = m7; m7 = m0; }
+	   else { m0 = m6; m6 = m7; m7 = m0; }
 	if (m7 > m8) { m7 = m7; m8 = m8; }
-	else { m0 = m7; m7 = m8; m8 = m0; }
+	   else { m0 = m7; m7 = m8; m8 = m0; }
 	if (m8 > m9) { m8 = m8; m9 = m9; }
-	else { m0 = m8; m8 = m9; m9 = m0; }
+	   else { m0 = m8; m8 = m9; m9 = m0; }
 
 	if (m1 > m2) { m1 = m1; m2 = m2; }
-	else { m0 = m1; m1 = m2; m2 = m0; }
+	   else { m0 = m1; m1 = m2; m2 = m0; }
 	if (m2 > m3) { m2 = m2; m3 = m3; }
-	else { m0 = m2; m2 = m3; m3 = m0; }
+	   else { m0 = m2; m2 = m3; m3 = m0; }
 	if (m3 > m4) { m3 = m3; m4 = m4; }
-	else { m0 = m3; m3 = m4; m4 = m0; }
+	   else { m0 = m3; m3 = m4; m4 = m0; }
 	if (m4 > m5) { m4 = m4; m5 = m5; }
-	else { m0 = m4; m4 = m5; m5 = m0; }
+	   else { m0 = m4; m4 = m5; m5 = m0; }
 	if (m5 > m6) { m5 = m5; m6 = m6; }
-	else { m0 = m5; m5 = m6; m6 = m0; }
+	   else { m0 = m5; m5 = m6; m6 = m0; }
 	if (m6 > m7) { m6 = m6; m7 = m7; }
-	else { m0 = m6; m6 = m7; m7 = m0; }
+	   else { m0 = m6; m6 = m7; m7 = m0; }
 	if (m7 > m8) { m7 = m7; m8 = m8; }
-	else { m0 = m7; m7 = m8; m8 = m0; }
+	   else { m0 = m7; m7 = m8; m8 = m0; }
 	if (m8 > m9) { m8 = m8; m9 = m9; }
-	else { m0 = m8; m8 = m9; m9 = m0; }
+	   else { m0 = m8; m8 = m9; m9 = m0; }
 
 	if (m1 > m2) { m1 = m1; m2 = m2; }
-	else { m0 = m1; m1 = m2; m2 = m0; }
+	   else { m0 = m1; m1 = m2; m2 = m0; }
 	if (m2 > m3) { m2 = m2; m3 = m3; }
-	else { m0 = m2; m2 = m3; m3 = m0; }
+	   else { m0 = m2; m2 = m3; m3 = m0; }
 	if (m3 > m4) { m3 = m3; m4 = m4; }
-	else { m0 = m3; m3 = m4; m4 = m0; }
+	   else { m0 = m3; m3 = m4; m4 = m0; }
 	if (m4 > m5) { m4 = m4; m5 = m5; }
-	else { m0 = m4; m4 = m5; m5 = m0; }
+	   else { m0 = m4; m4 = m5; m5 = m0; }
 	if (m5 > m6) { m5 = m5; m6 = m6; }
-	else { m0 = m5; m5 = m6; m6 = m0; }
+	   else { m0 = m5; m5 = m6; m6 = m0; }
 	if (m6 > m7) { m6 = m6; m7 = m7; }
-	else { m0 = m6; m6 = m7; m7 = m0; }
+	   else { m0 = m6; m6 = m7; m7 = m0; }
 	if (m7 > m8) { m7 = m7; m8 = m8; }
-	else { m0 = m7; m7 = m8; m8 = m0; }
+	   else { m0 = m7; m7 = m8; m8 = m0; }
 	if (m8 > m9) { m8 = m8; m9 = m9; }
-	else { m0 = m8; m8 = m9; m9 = m0; }
+	   else { m0 = m8; m8 = m9; m9 = m0; }
 
 	if (m1 > m2) { m1 = m1; m2 = m2; }
-	else { m0 = m1; m1 = m2; m2 = m0; }
+	   else { m0 = m1; m1 = m2; m2 = m0; }
 	if (m2 > m3) { m2 = m2; m3 = m3; }
-	else { m0 = m2; m2 = m3; m3 = m0; }
+	   else { m0 = m2; m2 = m3; m3 = m0; }
 	if (m3 > m4) { m3 = m3; m4 = m4; }
-	else { m0 = m3; m3 = m4; m4 = m0; }
+	   else { m0 = m3; m3 = m4; m4 = m0; }
 	if (m4 > m5) { m4 = m4; m5 = m5; }
-	else { m0 = m4; m4 = m5; m5 = m0; }
+	   else { m0 = m4; m4 = m5; m5 = m0; }
 	if (m5 > m6) { m5 = m5; m6 = m6; }
-	else { m0 = m5; m5 = m6; m6 = m0; }
+	   else { m0 = m5; m5 = m6; m6 = m0; }
 	if (m6 > m7) { m6 = m6; m7 = m7; }
-	else { m0 = m6; m6 = m7; m7 = m0; }
+	   else { m0 = m6; m6 = m7; m7 = m0; }
 	if (m7 > m8) { m7 = m7; m8 = m8; }
-	else { m0 = m7; m7 = m8; m8 = m0; }
+	   else { m0 = m7; m7 = m8; m8 = m0; }
 	if (m8 > m9) { m8 = m8; m9 = m9; }
-	else { m0 = m8; m8 = m9; m9 = m0; }
+	   else { m0 = m8; m8 = m9; m9 = m0; }
 
 	if (m1 > m2) { m1 = m1; m2 = m2; }
-	else { m0 = m1; m1 = m2; m2 = m0; }
+	   else { m0 = m1; m1 = m2; m2 = m0; }
 	if (m2 > m3) { m2 = m2; m3 = m3; }
-	else { m0 = m2; m2 = m3; m3 = m0; }
+	   else { m0 = m2; m2 = m3; m3 = m0; }
 	if (m3 > m4) { m3 = m3; m4 = m4; }
-	else { m0 = m3; m3 = m4; m4 = m0; }
+	   else { m0 = m3; m3 = m4; m4 = m0; }
 	if (m4 > m5) { m4 = m4; m5 = m5; }
-	else { m0 = m4; m4 = m5; m5 = m0; }
+	   else { m0 = m4; m4 = m5; m5 = m0; }
 	if (m5 > m6) { m5 = m5; m6 = m6; }
-	else { m0 = m5; m5 = m6; m6 = m0; }
+	   else { m0 = m5; m5 = m6; m6 = m0; }
 	if (m6 > m7) { m6 = m6; m7 = m7; }
-	else { m0 = m6; m6 = m7; m7 = m0; }
+	   else { m0 = m6; m6 = m7; m7 = m0; }
 	if (m7 > m8) { m7 = m7; m8 = m8; }
-	else { m0 = m7; m7 = m8; m8 = m0; }
+	   else { m0 = m7; m7 = m8; m8 = m0; }
 	if (m8 > m9) { m8 = m8; m9 = m9; }
-	else { m0 = m8; m8 = m9; m9 = m0; }
+	   else { m0 = m8; m8 = m9; m9 = m0; }
 
 	if (m1 > m2) { m1 = m1; m2 = m2; }
-	else { m0 = m1; m1 = m2; m2 = m0; }
+	   else { m0 = m1; m1 = m2; m2 = m0; }
 	if (m2 > m3) { m2 = m2; m3 = m3; }
-	else { m0 = m2; m2 = m3; m3 = m0; }
+	   else { m0 = m2; m2 = m3; m3 = m0; }
 	if (m3 > m4) { m3 = m3; m4 = m4; }
-	else { m0 = m3; m3 = m4; m4 = m0; }
+	   else { m0 = m3; m3 = m4; m4 = m0; }
 	if (m4 > m5) { m4 = m4; m5 = m5; }
-	else { m0 = m4; m4 = m5; m5 = m0; }
+	   else { m0 = m4; m4 = m5; m5 = m0; }
 	if (m5 > m6) { m5 = m5; m6 = m6; }
-	else { m0 = m5; m5 = m6; m6 = m0; }
+	   else { m0 = m5; m5 = m6; m6 = m0; }
 	if (m6 > m7) { m6 = m6; m7 = m7; }
-	else { m0 = m6; m6 = m7; m7 = m0; }
+	   else { m0 = m6; m6 = m7; m7 = m0; }
 	if (m7 > m8) { m7 = m7; m8 = m8; }
-	else { m0 = m7; m7 = m8; m8 = m0; }
+	   else { m0 = m7; m7 = m8; m8 = m0; }
 	if (m8 > m9) { m8 = m8; m9 = m9; }
-	else { m0 = m8; m8 = m9; m9 = m0; }
+	   else { m0 = m8; m8 = m9; m9 = m0; }
 
 	if (m1 > m2) { m1 = m1; m2 = m2; }
-	else { m0 = m1; m1 = m2; m2 = m0; }
+	   else { m0 = m1; m1 = m2; m2 = m0; }
 	if (m2 > m3) { m2 = m2; m3 = m3; }
-	else { m0 = m2; m2 = m3; m3 = m0; }
+	   else { m0 = m2; m2 = m3; m3 = m0; }
 	if (m3 > m4) { m3 = m3; m4 = m4; }
-	else { m0 = m3; m3 = m4; m4 = m0; }
+	   else { m0 = m3; m3 = m4; m4 = m0; }
 	if (m4 > m5) { m4 = m4; m5 = m5; }
-	else { m0 = m4; m4 = m5; m5 = m0; }
+	   else { m0 = m4; m4 = m5; m5 = m0; }
 	if (m5 > m6) { m5 = m5; m6 = m6; }
-	else { m0 = m5; m5 = m6; m6 = m0; }
+	   else { m0 = m5; m5 = m6; m6 = m0; }
 	if (m6 > m7) { m6 = m6; m7 = m7; }
-	else { m0 = m6; m6 = m7; m7 = m0; }
+	   else { m0 = m6; m6 = m7; m7 = m0; }
 	if (m7 > m8) { m7 = m7; m8 = m8; }
-	else { m0 = m7; m7 = m8; m8 = m0; }
+	   else { m0 = m7; m7 = m8; m8 = m0; }
 	if (m8 > m9) { m8 = m8; m9 = m9; }
-	else { m0 = m8; m8 = m9; m9 = m0; }
+	   else { m0 = m8; m8 = m9; m9 = m0; }
 	
 
 	return(m5);
