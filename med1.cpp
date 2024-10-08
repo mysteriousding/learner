7c7a170c2619d78782589aa12c2e569d858ee437
kunao_learner <15346777800@163.com>
2024-10-08 13:45:58 +0800

    Update med1.cpp

--- med1.cpp
+++ med1.cpp
@@ -18,17 +18,17 @@ int med(int x, int y, int z)
 	m3 = z;
 
 	if (m1 > m2) { m1 = m1; m2 = m2; }
-	else { m0 = m1; m1 = m2; m2 = m0; }
+	   else { m0 = m1; m1 = m2; m2 = m0; }
 	if (m2 > m3) { m2 = m2; m3 = m3; }
-	else { m0 = m2; m2 = m3; m3 = m0; }
+	   else { m0 = m2; m2 = m3; m3 = m0; }
 	if (m1 > m2) { m1 = m1; m2 = m2; }
-	else { m0 = m1; m1 = m2; m2 = m0; }
+	   else { m0 = m1; m1 = m2; m2 = m0; }
 	if (m2 > m3) { m2 = m2; m3 = m3; }
-	else { m0 = m2; m2 = m3; m3 = m0; }
+	   else { m0 = m2; m2 = m3; m3 = m0; }
 	if (m1 > m2) { m1 = m1; m2 = m2; }
-	else { m0 = m1; m1 = m2; m2 = m0; }
+	   else { m0 = m1; m1 = m2; m2 = m0; }
 	if (m2 > m3) { m2 = m2; m3 = m3; }
-	else { m0 = m2; m2 = m3; m3 = m0; }
+	   else { m0 = m2; m2 = m3; m3 = m0; }
 
 	return(m2);
 }
