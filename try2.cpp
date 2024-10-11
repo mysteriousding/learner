0ab7ab21e55742cccb0c76973ce6b6c4a17675cf
kunao_learner <15346777800@163.com>
2024-10-11 17:57:15 +0800

    Add files via upload

---
+++ try2.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+using namespace std;
+/*尝试一
+int main()
+{
+	int a, b;
+	a = 'a';
+	b = 'b';
+
+	cout << a << endl << b << endl;
+
+	return 0;
+}
+*/
+
+/*尝试二
+int main()
+{
+	char a, b;
+	a = 101;
+	b = 58;
+
+	cout << a << endl << b << endl;
+
+	return 0;
+}
+*/
+
+//尝试三
+int main()
+{
+	char a, b, c, d;
+	a = 'h';
+	b = 'e';
+	c = a - 32;
+	d = b - 32;
+
+	cout << c << endl << d << endl;
+
+	return 0;
+}
