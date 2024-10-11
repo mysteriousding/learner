e8448541a9c3e0c5832a6a49b7695b45d3047865
kunao_learner <15346777800@163.com>
2024-10-11 17:32:51 +0800

    Add files via upload

---
+++ try.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+using namespace std;
+/*   尝试一
+int main()
+{
+	int a;
+	//a = 234l;     十进制尝试
+	//a = 2387L     十进制尝试
+	
+	//a = 020;      八进制尝试
+	
+	//a = 0x20;     十六进制尝试
+	a = 0X20;     //十六进制尝试
+	cout << a << endl;
+
+	return 0;
+}
+*/
+
+//尝试二
+int main()
+{
+	int a, b, c, d, e, f, g, x, y;
+	x = 2147483646;
+	a = 2147483647;
+	b = 1;
+	c = 2;
+	d = 10;
+
+	e = a + b;
+	f = a + c;
+	g = a + d;
+	y = x + b;
+
+
+	cout << a << "+" << b << "=" << e << endl;
+	cout << a << "+" << c << "=" << f << endl;
+	cout << a << "+" << d << "=" << g << endl << endl;
+	cout << x << "+" << b << "=" << y << endl;
+
+	return 0;
+}
