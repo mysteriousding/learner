be7e3c4c7d19084cb1e24e675f8bdefe0848c4ff
kunao_learner <15346777800@163.com>
2024-10-11 18:33:45 +0800

    Add files via upload

---
+++ exe7.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+using namespace std;
+#define PRICE 30   
+//预处理指令，不用加分号  
+//“#define 宏名 字符串”形式，例如：#define PI 3.14159   
+int main()
+{
+	int num, total;
+	num = 10;
+	total = num * PRICE;
+	cout << "total=" << total << endl;
+
+	return 0;
+}
