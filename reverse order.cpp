1aca90f141c493e667612e9990ba7ec3b747d8b9
kunao_learner <15346777800@163.com>
2024-10-12 18:53:08 +0800

    Add files via upload

---
+++ reverse order.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int num, a, b, c, d, e, f, g, h, i, j, k;
+	cout << "请输入一个数字，让我为你将它颠倒" << endl;
+	cin >> num;
+	if (2.2e9 > num, num > 1e9)
+	{
+		a = num / 1e0% 10;
+		b = num / 1e1 % 10;
+		c = num / 1e2 % 10;
+		d = (num / 1e3) % 10;
+		e = (num / 1e4) % 10;
+		f = (num / 1e5) % 10;
+		g = (num / 1e6) % 10;
+		h = (num / 1e7) % 10;
+		i = (num / 1e8) % 10;
+		j = (num / 1e9) % 10;
+	
+	}
+
+	
+}
