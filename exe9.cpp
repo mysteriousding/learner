0e67f13d8a166ab7e2780ab72732b768b7eac87b
kunao_learner <15346777800@163.com>
2024-10-12 18:44:53 +0800

    Add files via upload

---
+++ exe9.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+using namespace std;
+/*第一题
+int main()
+{
+	char c1 = 'a', c2 = 'b', c3 = 'c', c4 = '\101', c5 = '\116';
+	cout << c1 << c2 << c3 << '\n';
+	cout << "\t\b" << c4 << '\t' << c5 << '\n';
+
+	return 0;
+}
+*/
+
+//第二题
+int main()
+{
+	char c1 = 'C', c2 = '+', c3 = '+';
+	cout << "I say:\"" << c1 << c2 << c3 << '\"';
+	cout << "\t\t" << "He says:\"C++ is very interesting!\"" << '\n';
+
+	return 0;
+}
