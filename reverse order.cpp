ab6a7b68615e2a3b4ff405503fd1f8abdcdb2d8d
kunao_learner <15346777800@163.com>
2024-10-12 19:01:16 +0800

    Update reverse order.cpp

--- reverse order.cpp
+++ reverse order.cpp
@@ -1,24 +1,68 @@
-﻿#include<iostream>
+#include<iostream>
 using namespace std;
+/*第一代
 int main()
 {
-	int num, a, b, c, d, e, f, g, h, i, j, k;
+	int num, num0, a, b, c, d, e, f, g, h, i, j;
 	cout << "请输入一个数字，让我为你将它颠倒" << endl;
 	cin >> num;
-	if (2.2e9 > num, num > 1e9)
-	{
-		a = num / 1e0% 10;
-		b = num / 1e1 % 10;
-		c = num / 1e2 % 10;
-		d = (num / 1e3) % 10;
-		e = (num / 1e4) % 10;
-		f = (num / 1e5) % 10;
-		g = (num / 1e6) % 10;
-		h = (num / 1e7) % 10;
-		i = (num / 1e8) % 10;
-		j = (num / 1e9) % 10;
+	num0 = num;
 	
+	a = num % 10;
+	num = num / 10;
+	b = num % 10;
+	num = num / 10;
+	c = num % 10;
+	num = num / 10;
+	d = num % 10;
+	num = num / 10;
+	e = num % 10;
+	num = num / 10;
+	f = num % 10;
+	num = num / 10;
+	g = num % 10;
+	num = num / 10;
+	h = num % 10;
+	num = num / 10;
+	i = num % 10;
+	num = num / 10;
+	j = num % 10;
+	num = num / 10;
+	
+	if (2.15e9 > num0 && num0 >= 1e9) { cout << a << b << c << d << e << f << g << h << i << j << endl; }
+	else if (1e9 > num0 && num0 >= 1e8) { cout << a << b << c << d << e << f << g << h << i << endl; }
+	else if (1e8 > num0 && num0 >= 1e7) { cout << a << b << c << d << e << f << g << h << endl; }
+	else if (1e7 > num0 && num0 >= 1e6) { cout << a << b << c << d << e << f << g << endl; }
+	else if (1e6 > num0 && num0 >= 1e5) { cout << a << b << c << d << e << f << endl; }
+	else if (1e5 > num0 && num0 >= 1e4) { cout << a << b << c << d << e << endl; }
+	else if (1e4 > num0 && num0 >= 1e3) { cout << a << b << c << d << endl; }
+	else if (1e3 > num0 && num0 >= 1e2) { cout << a << b << c << endl; }
+	else if (1e2 > num0 && num0 >= 1e1) { cout << a << b << endl; }
+	else { cout << a << endl; }
+
+	return 0;
+}
+*/
+
+//第二代
+int main()
+{
+	int num, a;
+	cout << "请输入一个数字，让我为你将它颠倒" << endl;
+	cin >> num;
+
+	while(true)
+	{
+		a = num % 10;
+		num = num / 10;
+
+		if (num == 0) 
+		{ 
+			cout << a;
+			break; 
+		}
+		   else { cout << a; }
 	}
 
-	
+	return 0;
 }
