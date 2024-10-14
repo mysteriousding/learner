8fb02f8eab6fe6e93a43b3e25d4cefd90edb1e8a
kunao_learner <15346777800@163.com>
2024-10-14 23:18:56 +0800

    Add files via upload

---
+++ work0.cpp
@@ -0,0 +1,35 @@
+﻿/*(1)
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long A, B;
+    cin >> A >> B;
+    cout << A << "+" << B << "=" << A + B << endl;
+
+    return 0;
+}
+*/
+
+/*(1)
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, a, i = 0;
+    cin >> n;
+    while (true)
+    {
+        a = n % 10;
+        n /= 10;
+        if (a == 0 && i == 0) {}
+        else
+        {
+            i++;
+            cout << a;
+        }
+        if (n == 0) { break; }
+    }
+    return 0;
+}
+*/
