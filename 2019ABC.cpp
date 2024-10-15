4eecc5ff1d55cf0631180fb536d12bafa8eab1cf
kunao_learner <15346777800@163.com>
2024-10-15 19:13:50 +0800

    Add files via upload

---
+++ 2019ABC.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, c = 1;
+	int n[10];
+	cin >> a;
+	while (true)
+	{
+		b = a % 26;
+		a /= 26;
+		n[c] = b;
+		if (a == 0) { break; }
+		c++;
+	}
+	for (c = c; c >= 0; c--)
+	{
+		cout << (char)(n[c] + 64);
+	}
+	cout << endl;
+
+	return 0;
+}
