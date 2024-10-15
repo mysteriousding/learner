9985db4601a1a960463d280df04a2eb252f117ce
kunao_learner <15346777800@163.com>
2024-10-15 22:25:52 +0800

    Add files via upload

---
+++ series0.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, c, a0, b0, c0, n = 3, m;
+	a = b = c = 1;
+	cin >> m;
+	while (true)
+	{
+		a0 = a%10000; b0 = b%10000; c0 = c%10000;
+		a = b0; b = c0; c = a0;
+
+		a = a + b + c;
+		n++;
+		if(n==m)
+		{
+			break;
+		}
+	}
+	cout << a % 10000 << endl;
+
+	return 0;
+}
