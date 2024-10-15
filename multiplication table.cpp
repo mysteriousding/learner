01599b683e58d739cee6a0d9612b185cb412d4f5
kunao_learner <15346777800@163.com>
2024-10-15 19:19:51 +0800

    Add files via upload

---
+++ multiplication table.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    for (a = 1; a <= 9; a++)
+    {
+        for (b = 1; b <= a; b++)
+        {
+            cout << a << "*" << b << "=" << a * b << "\t";
+            if (b==a) { cout << endl; }
+        }
+    }
+
+    return 0;
+}
