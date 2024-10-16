4cf41aeb767ea799f29d189e232b9abaa2611981
kunao_learner <15346777800@163.com>
2024-10-16 23:55:41 +0800

    Update series0.cpp

--- 2024/10/15/series0.cpp
+++ 2024/10/15/series0.cpp
@@ -1,4 +1,5 @@
-﻿#include<iostream>
+/*
+#include<iostream>
 using namespace std;
 int main()
 {
@@ -18,6 +19,33 @@ int main()
 		}
 	}
 	cout << a % 10000 << endl;
+
+	return 0;
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, c, a0, b0, c0, n = 3, m;
+	a = b = c = 1;
+	cout << "给定数列1,1,1,3,5,9,17,…，从第4项开始，每项都是前3项的和。求第X项的最后4位数字。" << endl;
+	cout << "请输入X为你计算" << endl;
+	cin >> m;
+	while (true)
+	{
+		a0 = a % 10000; b0 = b % 10000; c0 = c % 10000;
+		a = b0; b = c0; c = a0;
+
+		a = a + b + c;
+		n++;
+		if (n == m)
+		{
+			break;
+		}
+	}
+	cout << "X=" << a % 10000 << endl;
+	cout << "计算完毕，谢谢使用！" << endl;
 
 	return 0;
 }
