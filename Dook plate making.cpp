35a18ff7fa006b6e1e617fdfbb806b4445f5cc0e
kunao_learner <15346777800@163.com>
2024-10-16 23:38:58 +0800

    Add files via upload

---
+++ Dook plate making.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, n = 0, m, i = 1, i0;
+	cin >> m;
+	while(i<=m)
+	{
+		i0 = i;
+		while (true)
+		{
+			b = i % 10;
+			i /= 10;
+			if (b == 2)
+			{
+				n += 1;
+			}
+			if (i == 0)
+			{
+				break;
+			}
+		}
+		i = i0;
+		i++;
+	}
+	cout << n << endl;
+
+	return 0;
+}
