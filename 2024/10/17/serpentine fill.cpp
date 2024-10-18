40b3c6b4b73c6c0f07534bed11e0021bb4e426ae
kunao_learner <15346777800@163.com>
2024-10-18 13:29:31 +0800

    Update serpentine fill.cpp

--- 2024/10/17/serpentine fill.cpp
+++ 2024/10/17/serpentine fill.cpp
@@ -1,8 +1,9 @@
-﻿#include<iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
 	int a = 0, b, c, i, j, n, m;
+	cout << "如下图所示，小明用从1开始的正整数“蛇形”填充无限大的矩阵。\n容易看出矩阵第二行第二列中的数是5。请你计算矩阵中第20行第20列的数是多少？\n1 2 6 7 15 …\n3 5 8 14 …\n4 9 13 …\n10 12 …\n11 …" << endl;
 	cin >> m;
 	for (j = 0; j <= m - 1; j++)
 	{
