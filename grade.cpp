8cd6945eebbfecbae4ebe309b8ea8c559e713ff5
kunao_learner <15346777800@163.com>
2024-10-18 23:34:44 +0800

    Add files via upload

---
+++ grade.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int x = 0, y = 0;
+	int a[8];
+	for (int i = 0; i < 8; i++)
+	{
+
+		cin >> a[i];
+
+		if (a[i] >= 85)
+		{
+			x += 1;
+			y += 1;
+		}
+		else if (a[i] >= 60)
+		{
+			y += 1;
+		}
+	}
+
+		cout << "优秀率:" << (int)(((x / 8.0) * 100) + 0.5) << "%" << endl;
+		cout << "及格率:" << (int)(((y / 8.0) * 100) + 0.5) << "%" << endl;
+
+		return 0;
+}
