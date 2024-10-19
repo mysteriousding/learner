30c567095903ed97e921d655e5ee26c80519d9c2
kunao_learner <15346777800@163.com>
2024-10-19 23:40:55 +0800

    Add files via upload

---
+++ 10 to 2.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, c, i = 0;
+	cout << "请输入一个十进制数，让我为你将他转化为二进制。" << endl;
+	cin >> a;
+	c = a;
+	int arr[10];
+	for (i;; i++)
+	{
+		b = a % 2;
+		a /= 2;
+		arr[i] = b;
+
+		if (a == 0)break;
+	}
+	a = c;
+	cout << endl << a << "的二进制=";
+	for (; i >= 0; i--)
+	{
+		cout << arr[i];
+	}
+	cout << endl;
+
+	return 0;
+}
