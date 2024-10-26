68aac7993a2747fba9ade0667932db04d2f42e07
kunao_learner <15346777800@163.com>
2024-10-26 10:43:06 +0800

    Add files via upload

---
+++ pi.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	double a = 0;
+	int j = -1;
+	for (int i = 1;; i+=2)
+	{
+		j *= -1;
+		a += (1.0 / i) * j;
+		if ((1.0 / i) < 1e-7)break;
+	}
+
+	cout << "pi:" << 4 * a << endl;
+
+	return 0;
+}
