f1203abea40e7eb723f9e3a900ed78077f283fa3
kunao_learner <15346777800@163.com>
2024-10-26 10:44:41 +0800

    Add files via upload

---
+++ lmage0.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>      //三角形
+using namespace std;
+int main()
+{
+	int n;
+moon:
+	cin >> n;      //上限133
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = n + 1 - i; 0 < j; j--)
+		{
+			cout << " ";
+		}
+		for (int k = 2 * i - 1; 0 < k; k--)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	goto moon;
+
+	return 0;
+}
