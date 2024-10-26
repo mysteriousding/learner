a4997d2a969bac35ec0d2f7da03974e7d10cb1f0
kunao_learner <15346777800@163.com>
2024-10-26 10:46:09 +0800

    Add files via upload

---
+++ lmage1.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>            //正方形
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i <= n ; i++)
+	{
+		for (int j = 0; j <= n * 2+ 5; j++)
+		{
+			if (j < 5) { cout << " "; }
+			   else {cout << "*";}
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
