c04898bb99d8a1ce5852821a4363fe806f1684ef
kunao_learner <15346777800@163.com>
2024-11-02 23:45:58 +0800

    Add files via upload

---
+++ work14.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+    int n, h, k = 0;
+    int x[10] = { 0,0,0,0,0,0,0,0,0,0 };
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> h;
+        for (;;)
+        {
+            x[h % 10]++;
+            h /= 10;
+            if (h == 0)break;
+
+        }
+    }
+    for (int i = 0; i < 9; i++)
+    {
+        /*if (x[i] < x[i + 1])k = x[i + 1];*/
+        cout << " " << x[i];
+    }
+    /*cout << k << ":";
+    for (int i = 0; i < 10; i++)
+    {
+        if (x[i] == k)cout << " " << i;
+    }
+    cout << endl;*/
+
+    return 0;
+}
