9684820ab671f581eb78b2212069bb8764b54b49
kunao_learner <15346777800@163.com>
2024-11-03 21:34:35 +0800

    Add files via upload

---
+++ lmage5.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "请输入漏斗大小:";
+	cin >> n;
+	for (int k = 0; k < n; k++)
+	{
+		for (int i = 0; i <= k; i++)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j < (n - k) * 2 - 1; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+	for (int k = 1; k < n; k++)
+	{
+		for (int i = 0; i < n - k; i++)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j < k * 2 + 1; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
