32bdc411c10805d07485985585ba290417eda48f
kunao_learner <15346777800@163.com>
2024-11-03 21:38:52 +0800

    Update harmonic mean.cpp

--- 2024/11/3/harmonic mean.cpp
+++ 2024/11/3/harmonic mean.cpp
@@ -1,11 +1,11 @@
-﻿#include<iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
 	int n;
 	float m, k = 0;
 	cout << "N 个正数的算数平均是这些数的和除以 N，它们的调和平均是它们倒数的算数平均的倒数"<<endl;
-	cout << "请先输入N，再输入N个数；";
+	cout << "请先输入N，再输入N个数:";
 	cin >> n;
 	for (int i = 0; i < n; i++)
 	{
@@ -20,4 +20,3 @@ int main()
 
 	return 0;
 }
-
