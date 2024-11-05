bde25dacd71ae99b207c65e3ec6c2b1674daf09b
kunao_learner <15346777800@163.com>
2024-11-05 23:12:31 +0800

    Add files via upload

---
+++ ABC tower.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "输入一个 <=26 的数，将因此绘制字母塔。" << endl;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = n - i; j >= 0; j--)
+		{
+			cout << " ";
+		}
+
+		for (int j = i; j > 0; j--)
+		{
+			cout << (char)(j + 65);
+		}
+
+		for (int j = 0; j < i+1; j++)
+		{
+			cout << (char)(j + 65);
+		}
+
+		cout << endl;
+	}
+	
+
+	return 0;
+}
