445932c4051d4e17539dcdce242388e1bf848193
kunao_learner <15346777800@163.com>
2024-11-28 20:33:42 +0800

    Add files via upload

---
+++ start.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+#include<string>
+using namespace std;
+
+class student
+{
+public:
+	void SR();
+	void SC();
+private:
+	string MZ;
+	long long XH;
+	int NL;
+	string XB;
+	long long DH;
+	string DZ;
+};
+
+class V
+{
+public:
+	void SJ();
+	void JS();
+	void JG();
+	void KJ();
+private:
+	double length;
+	double width;
+	double height;
+	double TJ;
+};
