5a5ca897644a0ff0165ebab560a5e68a6823df8b
kunao_learner <15346777800@163.com>
2024-11-28 20:34:21 +0800

    Add files via upload

---
+++ partitioning.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include"start.h"
+using namespace std;
+int main()
+{
+    /*student t;
+    t.SR();
+    t.SC();*/
+    V v1,v2,v3;
+    v1.SJ(); 
+    v2.SJ(); 
+    v3.SJ();
+    v1.JS();
+    v2.JS(); 
+    v3.JS(); 
+    v1.JG();
+    v2.JG();
+    v3.JG();
+
+
+    return 0;
+}
