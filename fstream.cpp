4da10a1c0a510234987a307e127dca9f9fa1304c
kunao_learner <15346777800@163.com>
2024-12-01 22:55:15 +0800

    Add files via upload

---
+++ fstream.cpp
@@ -0,0 +1,43 @@
+﻿#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+int main()
+{
+	ofstream XY("xiangyu.txt"/*,ios::app*/);       //有 xiangyu.txt 就修改，没有就新建           //有 ios::app 是添加，没有是覆盖      //不只是 .txt 可以创建,如： .exe, .jpg
+	if (XY.is_open())
+		XY << "你好啊！"<< endl;
+	else
+	{
+		cerr << "错误！" << endl;
+		return 1;
+	}
+	XY.close();
+
+	string ai = "\0";
+	ifstream LB("xiangyu.txt");
+	if (LB.is_open())
+		LB >> ai;
+	else
+	{
+		cerr << "遗憾！" << endl;
+		return 1;
+	}
+	LB.close();
+	cout << ai << endl;
+
+
+	/*ifstream LB("fstream.cpp");
+
+	if (LB.is_open())
+		while (getline(LB, ai))
+			cout << ai << endl;
+	else
+	{
+		cerr << "Error!" << endl;
+		return 1;
+	}
+	LB.close();*/
+
+	return 0;
+}
