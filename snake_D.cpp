cba90bd218916209e6afb9a1f50bcfc1879fd7f4
kunao_learner <15346777800@163.com>
2024-12-02 23:13:44 +0800

    Add files via upload

---
+++ snake_D.cpp
@@ -0,0 +1,34 @@
+﻿#include"snake_.h"
+
+int main()
+{
+	srand(static_cast<unsigned int>(time(nullptr)));
+	CHANGDI KK; 
+	apple JPG(KK);
+	snake o1(18, 10, NULL, KK, 2), o2(19, 10, &o1, KK, 2), head(20, 10, &o2, KK, 1);
+	int time = 0, DF = 0;
+
+	while (1)
+	{
+		system("cls"); 
+		cout << "时间: " << time << endl;
+		cout << "得分: " << DF << endl;
+		KK.HZ();
+		JPG.H(KK);
+		JIANCHE(head);
+		if (!head.CXsnakeL()) { Sleep(500); system("cls");  break; }
+		head.KZ(head, JPG, KK, DF);
+		time++;
+	}
+
+	
+	cout << "游戏结束了！" << endl;
+	cout << "存活时间: " << time << endl;
+	cout << "本局得分: " << DF << endl;
+	cout << "\n再来一局吧！" << endl;
+
+
+	system("pause");
+
+	return 0;
+}
