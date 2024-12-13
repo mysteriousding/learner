ce2e5ca9a6f1754a8ee6e3cff73028621b0e7068
kunao_learner <15346777800@163.com>
2024-12-13 22:06:26 +0800

    Update 24work5.cpp

--- 2024/12/9/24work5.cpp
+++ 2024/12/9/24work5.cpp
@@ -1,4 +1,4 @@
-﻿/*(21)h0029. 数组排序
+/*(21)h0029. 数组排序
 
 //给定一个长度为n的数组a以及两个整数 l 和 r，请你编写一个函数，void sort(int a[], int l, int r)，将a[l] ~ a[r]从小到大排序。
 //
@@ -138,7 +138,212 @@ int main()
 //6! = 720
 //8! = 40320
 
+// #include<iostream>
+// using namespace std;
+// long long Fact(int  n)
+// {
+//     int  i;
+//     long long result = 1;
+//     for (i=2; i<=n; i++)
+//         result *= i;
+//     return result;
+// }
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     for(int i=2;i<=n;i+=2)
+//         cout<<i<<"! = "<<Fact(i)<<endl;
 
+//     return 0;
+// }
+
+
+
+// #include<iostream>
+// #include<string>
+// using namespace std;
+// string sum = "1xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+// void Fact(int n)
+// {
+//     string m[5];
+//     int n0=n;
+//     for(int c=0;c<2;c++)
+//     {
+//         n=n0-c;
+//         for(int i=0;i<5;i++)
+//             m[i]="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+//         int r=0;
+//         while(n!=0)
+//         {
+//             int d=n%10;
+//             n/=10;
+
+//             int j=0,k=0;
+//             while(j<r)
+//                 m[r][j++]='0';
+//             j=0;
+//             while(sum[j]!='x'||k!=0)
+//             {
+//                 m[r][j+r]=((sum[j]-((sum[j]=='x')?'x':'0'))*d+k)%10+'0';
+//                 k=((sum[j]-((sum[j]=='x')?'x':'0'))*d+k)/10;
+//                 j++;
+//             }
+//             r++;
+//         }
+
+//         for(int i=0;i<r;i++)
+//         {
+//             int j=0,k=0;
+//             while(m[4][j]!='x'||m[i][j]!='x'||k!=0)
+//             {
+//                 int cw=((m[4][j]-((m[4][j]=='x')?'x':'0'))+(m[i][j]-((m[i][j]=='x')?'x':'0'))+k);
+//                 m[4][j]=cw%10+'0';
+//                 k=cw/10;
+//                 j++;
+//             }
+//         }
+//         sum=m[4];
+//     }
+// }
+// int main()
+// {
+//     int n,j;
+//     string m0;
+//     cin>>n;
+//     for(int i=2;i<=n;i+=2)
+//     {
+//         cout<<i<<"! = ";
+//         Fact(i);
+//         j=0;
+//         m0="";
+//         while(sum[j]!='x')
+//             m0=sum[j++]+m0;
+//         cout<<m0<<endl;
+//     }
+
+//     return 0;
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// double  Fact(int  n)
+// {
+//     int  i;
+//     double result = 1;
+//     for (i=2; i<=n; i++)
+//         result *= i;
+//     return result;
+// }
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     for(int i=2;i<=n;i+=2)
+//         cout<<i<<"! = "<<Fact(i)<<endl;
+
+//     return 0;
+// }
+
+
+
+// #include<iostream>
+// #include<string>
+// using namespace std;
+
+// long long Fact(int  n)
+// {
+//     int  i;
+//     long long result = 1;
+//     for (i=2; i<=n; i++)
+//         result *= i;
+//     return result;
+// }
+
+// double  fact(int  n)
+// {
+//     int  i;
+//     double result = 1;
+//     for (i=2; i<=n; i++)
+//         result *= i;
+//     return result;
+// }
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     for(int i=2;i<n&&i<=10;i+=2)
+//         cout<<i<<"! = "<<Fact(i)<<endl;
+//     for(int i=12;i<n;i+=2)
+//         cout<<i<<"! = "<<fact(i)<<endl;
+
+//     return 0;
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// int Fact(int *arr,int  n)
+// {
+//     int j, temp, mod = 0;
+//     for (j = 1; j < 10000; j++)
+//     {
+//         temp = arr[j] * n + mod;
+//         mod = temp / 10;
+//         arr[j] = temp % 10;
+//     }
+//     for (j = 9999; j > 0; j--)
+//         if (arr[j] != 0)
+//             return j;
+// }
+//  int main()
+//  {
+//      int n, j;
+//      cin >> n;
+//      int* arr = new int[10000] { 1, 1 };
+//      for (int k = 1; k <= n; k++)
+//      {
+//          j = Fact(arr, k);
+//          if(k%2==0)
+//          {
+//              cout << k << "! = ";
+//              while (j)
+//                  cout << arr[j--];
+
+//              cout << endl;
+//          }
+//      }
+//      delete [] arr;
+
+//      return 0;
+//  }
+
+
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+double  Fact(int  n)
+{
+    int  i;
+    double result = 1;
+    for (i=2; i<=n; i++)
+        result *= i;
+    return result;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=2;i<=n;i+=2)
+        cout<<i<<"! = "<<setiosflags(ios::fixed)<<setprecision(0)<<Fact(i)<<endl;
+
+    return 0;
+}
 */
 
 /*(24)从1到n之间所有奇数的阶乘值
