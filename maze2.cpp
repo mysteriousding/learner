5939fc9a924e91b4fb3f7ebaecdf2fd0246215fa
kunao_learner <15346777800@163.com>
2024-12-29 22:05:43 +0800

    Add files via upload

---
+++ maze2.cpp
@@ -0,0 +1,33 @@
+﻿#include"maze2.h";
+
+int main()
+{
+    srand((unsigned)time(nullptr));
+
+    for (int i = 0; i < 32; i++)
+    {
+        tu t;
+        system("cls");
+        cout << "第" << i + 1 << "关" << endl; 
+        t.huizhi(); 
+        do {
+
+            t.kunzhi();
+            system("cls");
+            cout << "第" << i + 1 << "关" << endl;
+            t.huizhi();
+
+        } while (tu::live);
+
+        Sleep(1000);
+        cout << "\n过关了！你真厉害！" << endl;
+        cout << "继续挑战下一关吧！!" << endl;
+        system("pause");
+    }
+
+    cout << "\n恭喜你！游戏胜利了！！！" << endl;
+    cout << "再来一局吧！!" << endl;
+    system("pause");
+
+    return 0;
+}
