cae58054454f6941161822668fd7ad03809ad2a1
kunao_learner <15346777800@163.com>
2025-01-24 22:32:23 +0800

    Update L-work2.1.cpp

--- 2025/1/23/L-work2.1.cpp
+++ 2025/1/23/L-work2.1.cpp
@@ -219,8 +219,6 @@ int main()
 //0001 15 0.600 100.000
 //5551 4 0.750 100.000
 
-
-*/
 #include<iostream>
 #include<iomanip>
 #include<string>
@@ -229,126 +227,343 @@ int main()
 using namespace std;
 struct man
 {
-    string h[8];
+    string h[9];
     int k;
     int f;
     int s;
     int p;
     vector<string>d;
-    friend istream& operator>>(istream& in, man& r);
+    friend istream& operator>>(istream& in,man& r);
 };
-istream& operator>>(istream& in, man& r)
+istream& operator>>(istream& in,man& r)
 {
-    int x = 2, y = 0;
-    in >> r.h[y++];
-    in >> r.h[y];
-    if (r.h[y] != "-1")y++, x--;
-    in >> r.h[y];
-    if (r.h[y] != "-1")y++, x--;
+    int x=2,y=0;
+    in>>r.h[y++];
+    in>>r.h[y];
+    if(r.h[y]!="-1")y++,x--;
+    in>>r.h[y];
+    if(r.h[y]!="-1")y++,x--;
 
-    in >> r.k;
-    r.k += y;
-    for (int i = y; i < r.k; i++)
-        in >> r.h[i];
-    sort(r.h, r.h + r.k);
-    in >> r.f >> r.s;
+    in>>r.k;
+    r.k+=y;
+    for(int i=y;i<r.k;i++)
+        in>>r.h[i];
+    sort(r.h,r.h+r.k);
+    in>>r.f>>r.s;
     return in;
 }
 struct jia
 {
-    string min = "9999";
-    int shu = 0;
+    string min="9999";
+    int shu=0;
     vector<string>d;
-    double f = 0;
-    double s = 0;
+    double f=0;
+    double s=0;
 };
-ostream& operator<<(ostream& out, jia& r)
+ostream& operator<<(ostream& out,jia& r)
 {
-    out << r.min << " " << r.shu << " " << r.f << " " << r.s;
+    out<<r.min<<" "<<r.shu<<" "<<r.f<<" "<<r.s;
     return out;
 }
-bool pd(man& a, man& b)
+bool pd(man& a,man& b)
 {
-    return a.p < b.p;
+    return a.p<b.p;
 }
-bool pd2(jia& x, jia& y)
+bool pd2(jia& x,jia& y)
 {
-    if (fabs(x.s - y.s) < 1e-3)return x.min < y.min;
-    else return x.s > y.s;
+    if(fabs(x.s-y.s)<1e-4)return x.min<y.min;
+    else return x.s>y.s;
 }
 int main()
 {
-    int n, l;
+    int n,l;
     bool u;
-    cin >> n;
+    cin>>n;
     vector<man>m(n);
-    for (int i = 0; i < n; i++)
+    for(int i=0;i<n;i++)
     {
-        cin >> m[i];
-        m[i].p = i;
+        cin>>m[i];
+        m[i].p=i;
     }
-    for (int i = 0; i < n; i++)
-        for (int j = i + 1; j < n; j++)
-            if (m[i].p != m[j].p)
+    for(int i=0;i<n;i++)
+        for(int j=i+1;j<n;j++)
+            if(m[i].p!=m[j].p)
             {
-                u = 0;
-                for (int a = 0; a < m[i].k; a++)
+                u=0;
+                for(int a=0;a<m[i].k;a++)
                 {
-                    for (int b = 0; b < m[j].k; b++)
+                    for(int b=0;b<m[j].k;b++)
                     {
-                        if (m[i].h[a] < m[j].h[b])break;
-                        if (m[i].h[a] == m[j].h[b])
+                        if(m[i].h[a]<m[j].h[b])break;
+                        if(m[i].h[a]==m[j].h[b])
                         {
-                            u = 1;
+                            u=1;
                             m[i].d.push_back(m[i].h[a]);
                             m[j].d.push_back(m[j].h[b]);
                         }
                     }
                 }
-                if (u)
+                if(u)
                 {
-                    l = m[j].p;
-                    for (int c = 0; c < n; c++)
-                        if (m[c].p == l)
-                            m[c].p = m[i].p;
+                    l=m[j].p;
+                    for(int c=0;c<n;c++)
+                        if(m[c].p==l)
+                            m[c].p=m[i].p;
                 }
             }
-    sort(m.begin(), m.end(), pd);
-    l = m[0].p;
-    int num = 1;
-    vector<jia>v(n / 2);
-    for (int i = 0; i < n; i++)
+    sort(m.begin(),m.end(),pd);
+    l=m[0].p;
+    int num=1;
+    vector<jia>v(n);
+    for(int i=0;i<n;i++)
     {
-        if (l != m[i].p)
-            num++, l = m[i].p;
-        for (int j = 0; j < m[i].k; j++)
-            if (v[num - 1].min > m[i].h[j])
-                v[num - 1].min = m[i].h[j];
-        v[num - 1].shu += m[i].k;
-        v[num - 1].d.insert(v[num - 1].d.end(), m[i].d.begin(), m[i].d.end());
-        v[num - 1].f += m[i].f;
-        v[num - 1].s += m[i].s;
+        if(l!=m[i].p)
+            num++,l=m[i].p;
+        for(int j=0;j<m[i].k;j++)
+            if(v[num-1].min>m[i].h[j])
+                v[num-1].min=m[i].h[j];
+        v[num-1].shu+=m[i].k;
+        v[num-1].d.insert(v[num-1].d.end(),m[i].d.begin(),m[i].d.end());
+        v[num-1].f+=m[i].f;
+        v[num-1].s+=m[i].s;
     }
     int sum;
     string f;
-    for (int i = 0; i < num; i++)
+    for(int i=0;i<num;i++)
     {
-        sort(v[i].d.begin(), v[i].d.end());
-        if (!v[i].d.empty())
+        sort(v[i].d.begin(),v[i].d.end());
+        if(!v[i].d.empty())
         {
-            sum = 1, f = v[i].d[0];
-            for (size_t j = 0; j < v[i].d.size(); j++)
-                if (f != v[i].d[j])
-                    sum++, f = v[i].d[j];
-            v[i].shu -= v[i].d.size() - sum;
+            sum=1,f=v[i].d[0];
+            for(size_t j=0;j<v[i].d.size();j++)
+                if(f!=v[i].d[j])
+                    sum++,f=v[i].d[j];
+            v[i].shu-=v[i].d.size()-sum;
         }
-        v[i].f /= v[i].shu, v[i].s /= v[i].shu;
+        v[i].f/=v[i].shu,v[i].s/=v[i].shu;
     }
-    sort(v.begin(), v.begin() + num, pd2);
-    cout << num << endl;
-    cout << setiosflags(ios::fixed) << setprecision(3);
-    for (int i = 0; i < num; i++)
-        cout << v[i] << endl;
+    sort(v.begin(),v.begin()+num,pd2);
+    cout<<num<<endl;
+    cout<<setiosflags(ios::fixed)<<setprecision(3);
+    for(int i=0;i<num;i++)
+        cout<<v[i]<<endl;
 
     return 0;
 }
+*/
+
+/*(4)Tree Traversals Again
+
+An inorder binary tree traversal can be implemented in a non-recursive way with a stack. 
+For example, suppose that when a 6-node binary tree (with the keys numbered from 1 to 6) is traversed, the stack operations are: 
+push(1); push(2); push(3); pop(); pop(); push(4); pop(); pop(); push(5); push(6); pop(); pop().  
+Then a unique binary tree (shown in Figure 1) can be generated from this sequence of operations. 
+Your task is to give the postorder traversal sequence of this tree.
+
+
+Figure 1
+Input Specification:
+Each input file contains one test case. 
+For each case, the first line contains a positive integer N (≤30) which is the total number of nodes in a tree (and hence the nodes are numbered from 1 to N). 
+Then 2N lines follow, each describes a stack operation in the format:
+"Push X" where X is the index of the node being pushed onto the stack; or "Pop" meaning to pop one node from the stack.
+
+Output Specification:
+For each test case, print the postorder traversal sequence of the corresponding tree in one line. 
+A solution is guaranteed to exist. 
+All the numbers must be separated by exactly one space, and there must be no extra space at the end of the line.
+
+Sample Input:
+6
+Push 1
+Push 2
+Push 3
+Pop
+Pop
+Push 4
+Pop
+Pop
+Push 5
+Push 6
+Pop
+Pop
+Sample Output:
+3 4 2 6 5 1
+
+/*
+再次遍历树形
+
+中序二叉树遍历可以通过堆栈以非递归方式实现。
+例如，假设当遍历一棵 6 节点二叉树（键编号为 1 到 6）时，堆栈操作为：
+push(1); push(2); push(3); pop(); pop(); push(4); pop(); pop(); push(5); push(6); pop(); pop().
+然后，可以从这一系列操作中生成一个唯一的二叉树（如图 1 所示）。
+你的任务是给出这个树的后序遍历序列。
+https://images.ptausercontent.com/30
+图 1
+输入规格：
+每个输入文件都包含一个测试用例。
+对于每种情况，第一行都包含一个正整数 N （≤30），这是树中的节点总数（因此节点的编号从 1 到 N）。
+然后是 2N 行，每行描述一个堆栈操作，格式如下：
+“Push X”，其中 X 是被推送到堆栈上的节点的索引;或 “Pop” 表示从堆栈中弹出一个节点。
+
+输出规格：
+对于每个测试用例，在一行中打印相应树的后序遍历序列。
+解决方案是肯定存在的。
+所有数字必须用一个空格分隔，并且行尾不能有多余的空格。
+
+示例输入：
+6
+Push 1
+Push 2
+Push 3
+Pop
+Pop
+Push 4
+Pop
+Pop
+Push 5
+Push 6
+Pop
+Pop
+
+示例输出：
+3 4 2 6 5 1
+*
+
+#include<iostream>
+#include<string>
+using namespace std;
+struct Tree
+{
+    int num;
+    bool l=1;
+    Tree*left=NULL;
+    Tree*right=NULL;
+    Tree*q=NULL;
+}*head=NULL,*p=NULL;
+bool u=0;
+void bl(Tree* p)
+{
+    if(!p)return;
+    bl(p->left);
+    bl(p->right);
+    if(u)cout<<" ";
+    cout<<p->num;
+    u=1;
+    delete p;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    n*=2;
+    string m;
+    while(n--)
+    {
+        cin>>m;
+        if(m=="Push")
+        {
+            Tree*node=new Tree;
+            cin>>node->num;
+            if(!head)
+            {
+                p=head=node;
+                continue;
+            }
+            if(p->l)
+            {
+                p->left=node;
+                node->q=p;
+                p=node;
+            }
+            else
+            {
+                p->right=node;
+                node->q=p;
+                p=node;
+            }
+        }
+        else
+        {
+            while(!p->l)
+                p=p->q;
+            p->l=0;
+            //cout<<p->num<<" ";
+        }
+    }
+    bl(head);
+
+    return 0;
+}
+*/
+
+//代码排版
+//
+//某编程大赛中设计有一个挑战环节，选手可以查看其他选手的代码，发现错误后，提交一组测试数据将对手挑落马下。
+//为了减小被挑战的几率，有些选手会故意将代码写得很难看懂，比如把所有回车去掉，提交所有内容都在一行的程序，令挑战者望而生畏。
+//
+//为了对付这种选手，现请你编写一个代码排版程序，将写成一行的程序重新排版。
+//当然要写一个完美的排版程序可太难了，这里只简单地要求处理C语言里的for、while、if-else这三种特殊结构，而将其他所有句子都当成顺序执行的语句处理。
+//输出的要求如下：
+//
+//默认程序起始没有缩进；每一级缩进是 2 个空格；
+//每行开头除了规定的缩进空格外，不输出多余的空格；
+//顺序执行的程序体是以分号“;”结尾的，遇到分号就换行；
+//在一对大括号“{”和“}”中的程序体输出时，两端的大括号单独占一行，内部程序体每行加一级缩进，即：
+//
+//{
+//  程序体
+//}
+//
+//for的格式为：
+//for (条件) {
+//  程序体
+//}
+//
+//while的格式为：
+//while (条件) {
+//  程序体
+//}
+//
+//if-else的格式为：
+//if (条件) {
+//  程序体
+//}
+//
+//else {
+//  程序体
+//}
+//
+//输入格式：
+//输入在一行中给出不超过 331 个字符的非空字符串，以回车结束。
+//题目保证输入的是一个语法正确、可以正常编译运行的 main 函数模块。
+//
+//输出格式：
+//按题面要求的格式，输出排版后的程序。
+//
+//输入样例：
+//int main()  {int n, i;  scanf("%d", &n);if( n>0)n++;else if (n<0) n--; else while(n<10)n++; for(i=0;  i<n; i++ ){ printf("n=%d\n", n);}return  0; }
+//输出样例：
+//int main()
+//{
+//  int n, i;
+//  scanf("%d", &n);
+//  if ( n>0) {
+//    n++;
+//  }
+//  else {
+//    if (n<0) {
+//      n--;
+//    }
+//    else {
+//      while (n<10) {
+//        n++;
+//      }
+//    }
+//  }
+//  for (i=0;  i<n; i++ ) {
+//    printf("n=%d\n", n);
+//  }
+//  return  0;
+//}
+
