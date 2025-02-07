0f24d177f01320bcff5e5ca869ab97e1014c7c0a
kunao_learner <15346777800@163.com>
2025-02-08 00:11:23 +0800

    Update L-work4.1.cpp

--- 2025/2/7/L-work4.1.cpp
+++ 2025/2/7/L-work4.1.cpp
@@ -109,12 +109,26 @@ int main()
 }
 */
 
-/*
+/*(4)一元多项式的乘法与加法运算
 
+//设计函数分别求两个一元多项式的乘积与和。
+//
+//输入格式:
+//输入分2行，每行分别先给出多项式非零项的个数，再以指数递降方式输入一个多项式非零项系数和指数（绝对值均为不超过1000的整数）。
+//数字间以空格分隔。
+//
+//输出格式:
+//输出分2行，分别以指数递降方式输出乘积多项式以及和多项式非零项的系数和指数。
+//数字间以空格分隔，但结尾不能有多余空格。
+//零多项式应输出0 0。
+//
+//输入样例:
+//4 3 4 -5 2  6 1  -2 0
+//3 5 20  -7 4  3 1
+//输出样例:
+//15 24 -25 22 30 21 -10 20 -21 8 35 6 -33 5 14 4 -15 3 18 2 -6 1
+//5 20 -4 4 -5 2 9 1 -2 0
 
-
-*/
-#include<iostream>
 #include<iostream>
 #include<algorithm>
 #include<vector>
@@ -124,60 +138,100 @@ struct e
     int a;
     int b;
 };
-bool pd(e& x, e& y)
+bool pd(e& x,e& y)
 {
-    return x.b > y.b;
+    return x.b>y.b;
 }
 int main()
 {
-    int n, m, i, j, k;
+    int n,m,i,j,k;
     bool u;
-    cin >> n;
+    cin>>n;
     vector<e>x(n);
-    for (i = 0; i < n; i++)
-        cin >> x[i].a >> x[i].b;
-    cin >> m;
+    for(i=0;i<n;i++)
+        cin>>x[i].a>>x[i].b;
+    cin>>m;
     vector<e>y(m);
-    for (j = 0; j < m; j++)
-        cin >> y[j].a >> y[j].b;
-    vector<e>z(n * m);
-    for (i = 0; i < n; i++)
-        for (j = 0; j < m; j++)
-            z[i * m + j].a = x[i].a * y[j].a, z[i * m + j].b = x[i].b + y[j].b;
-    sort(z.begin(), z.end(), pd);
-    for (i = 0; i < n * m - 1; i++)
-        if (z[i].b == z[i + 1].b)
-            z[i + 1].a += z[i].a, z[i].a = 2025;
-    u = 1;
-    for (i = 0; i < n * m; i++)
-        if (z[i].a <= 2000 && z[i].a)
+    for(j=0;j<m;j++)
+        cin>>y[j].a>>y[j].b;
+    vector<e>z(n*m);
+    for(i=0;i<n;i++)
+        for(j=0;j<m;j++)
+            z[i*m+j].a=x[i].a*y[j].a,z[i*m+j].b=x[i].b+y[j].b;
+    sort(z.begin(),z.end(),pd);
+    for(i=0;i<n*m-1;i++)
+        if(z[i].b==z[i+1].b)
+            z[i+1].a+=z[i].a,z[i].b=2025;
+    u=1;
+    for(i=0;i<n*m;i++)
+        if(z[i].b<=2000&&z[i].a)
         {
-            if (i)cout << " ";
-            cout << z[i].a << " " << z[i].b;
-            u = 0;
+            if(!u)cout<<" ";
+            cout<<z[i].a<<" "<<z[i].b;
+            u=0;
         }
-    if (u)cout << "0 0";
-    cout << endl;
+    if(u)cout<<"0 0";
+    cout<<endl;
 
-    z.resize(n + m);
-    for (i = 0, j = 0, k = 0; i < n && j < m; k++)
-        if (x[i].b == y[j].b)z[k].b = x[i].b, z[k].a = x[i++].a + y[j++].a;
-        else if (x[i].b > y[j].b)z[k].b = x[i].b, z[k].a = x[i++].a;
-        else z[k].b = y[j].b, z[k].a = y[j++].a;
-    for (; i < n; i++)
-        z[k].b = x[i].b, z[k++].a = x[i++].a;
-    for (; j < m; j++)
-        z[k].b = y[j].b, z[k++].a = y[j++].a;
-    u = 1;
-    for (i = 0; i < k; i++)
-        if (z[i].a)
+    z.resize(n+m);
+    for(i=0,j=0,k=0;i<n&&j<m;k++)
+        if(x[i].b==y[j].b)z[k].b=x[i].b,z[k].a=x[i++].a+y[j++].a;
+        else if(x[i].b>y[j].b)z[k].b=x[i].b,z[k].a=x[i++].a;
+        else z[k].b=y[j].b,z[k].a=y[j++].a;
+    for(;i<n;i++)
+        z[k].b=x[i].b,z[k++].a=x[i].a;
+    for(;j<m;j++)
+        z[k].b=y[j].b,z[k++].a=y[j].a;
+    u=1;
+    for(i=0;i<k;i++)
+        if(z[i].a)
         {
-            if (i)cout << " ";
-            cout << z[i].a << " " << z[i].b;
-            u = 0;
+            if(!u)cout<<" ";
+            cout<<z[i].a<<" "<<z[i].b;
+            u=0;
         }
-    if (u)cout << "0 0";
-    cout << endl;
+    if(u)cout<<"0 0";
+    cout<<endl;
+
+    return 0;
+}
+*/
+
+//矩阵运算
+//
+//给定一个n×n的方阵，本题要求计算该矩阵除副对角线、最后一列和最后一行以外的所有元素之和。
+//副对角线为从矩阵的右上角至左下角的连线。
+//
+//输入格式:
+//输入第一行给出正整数n（1<n≤10）；随后n行，每行给出n个整数，其间以空格分隔。
+//
+//输出格式:
+//在一行中给出该矩阵除副对角线、最后一列和最后一行以外的所有元素之和。
+//
+//输入样例:
+//4
+//2 3 4 1
+//5 6 1 1
+//7 1 8 1
+//1 1 1 1
+//输出样例:
+//35
+
+#include<iostream>
+using namespace std;
+int a[11][11];
+int main()
+{
+    int n, sum = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> a[i][j];
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - 1; j++)
+            if (i + j != n - 1)
+                sum += a[i][j];
+    cout << sum << endl;
 
     return 0;
 }
