ac5ac5ef203720f57f77d4ea54ce93b1a9442ed2
kunao_learner <15346777800@163.com>
2025-03-11 23:32:17 +0800

    Add files via upload

---
+++ L-tri.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int a[21][21] = { 0 };
+int main()
+{
+    int n, j = 1;
+    cin >> n;
+    for (int i = 1; i <= n/2 ; i++)
+    {
+        for (int k = i; k <= n - i; k++)
+            a[i][k] = j++;
+        //for (int k =n-i; k >i; k--)
+        //    a[n-i-k][k] = j++;
+        //for (int k = n - i - 2; k > i; k--)
+        //    a[k][i] = j++;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int k = 1; k <= n - i; k++)
+        {
+            if (k)cout << " ";
+            cout << a[i][k];
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
