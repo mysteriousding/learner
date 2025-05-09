4a738e2b8a56e5603b21777f70f45983413c0799
kunao_learner <15346777800@163.com>
2025-05-09 23:46:42 +0800

    Add files via upload

---
+++ sjjg_sy4(3).cpp
@@ -0,0 +1,48 @@
+﻿/*
+3．散列表的应用（可根据实际情况进行调整）选择合适散列函数和冲突处理方法，编程实现QQ账户的申请与登录。
+*/
+#include<iostream>
+#include<string>
+using namespace std;
+#define N 1000
+
+typedef struct
+{
+	int n;
+	string m;
+}ys;
+
+class slb
+{
+	ys a[N];
+
+	slb()
+	{
+		for (int i = 0; i < N; i++)
+			a[i].n = -1;
+	}
+
+	int hx(int x);
+	int cz(int x);
+	bool cr();
+	bool sc();
+	bool xg();
+};
+
+int slb::hx(int x)
+{
+	return x % N;
+}
+
+int slb::cz(int x)
+{
+	int y = hx(x);
+	return x % N;
+}
+
+int main()
+{
+	cout << -7 % 5 << endl;
+
+
+}
