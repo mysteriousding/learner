a77a23c59ae99f3f5d65810c5f96bc95f65035f7
kunao_learner <15346777800@163.com>
2025-05-09 23:49:52 +0800

    Update sjjg_sy4(3).cpp

--- 2025/5/9/sjjg_sy4(3).cpp
+++ 2025/5/9/sjjg_sy4(3).cpp
@@ -1,4 +1,4 @@
-﻿/*
+/*
 3．散列表的应用（可根据实际情况进行调整）选择合适散列函数和冲突处理方法，编程实现QQ账户的申请与登录。
 */
 #include<iostream>
@@ -15,11 +15,31 @@ typedef struct
 class slb
 {
 	ys a[N];
+	int zs;
 
 	slb()
 	{
 		for (int i = 0; i < N; i++)
 			a[i].n = -1;
+		int j;
+		bool u;
+		for (int i = N; i > 1; i--)
+		{
+			u = 1;
+			if (i % 2 == 0 && i > 2 || i % 3 == 0)u = 0;
+			if (u)
+				for (j = 5; j * j <= i; j += 2)
+					if (i % j == 0)
+					{
+						u = 0;
+						break;
+					}
+			if (u)
+			{
+				zs = j;
+				break;
+			}
+		}
 	}
 
 	int hx(int x);
@@ -31,18 +51,19 @@ class slb
 
 int slb::hx(int x)
 {
-	return x % N;
+	return x % zs;
 }
 
 int slb::cz(int x)
 {
-	int y = hx(x);
-	return x % N;
+	int y0 = hx(x),y=y0;
+	for (int i = 0; a[y].n != x && a[y].n != -1; i++)
+		y = (y0 + N + (i * i) % N * (i % 2 ? 1 : -1)) % N;
+	return y;
 }
 
 int main()
 {
-	cout << -7 % 5 << endl;
 
 
 }
