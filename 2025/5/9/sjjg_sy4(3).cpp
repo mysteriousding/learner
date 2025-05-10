ef1d4f0350c1388b27fa71d39322f12fb8487dc5
kunao_learner <15346777800@163.com>
2025-05-10 10:28:47 +0800

    Update sjjg_sy4(3).cpp

--- 2025/5/9/sjjg_sy4(3).cpp
+++ 2025/5/9/sjjg_sy4(3).cpp
@@ -10,25 +10,24 @@ typedef struct
 {
 	int n;
 	string m;
+	string name;
 }ys;
 
 class slb
 {
-	ys a[N];
-	int zs;
-
+public:
 	slb()
 	{
 		for (int i = 0; i < N; i++)
 			a[i].n = -1;
-		int j;
+		
 		bool u;
 		for (int i = N; i > 1; i--)
 		{
 			u = 1;
-			if (i % 2 == 0 && i > 2 || i % 3 == 0)u = 0;
+			if ((i % 2 == 0 && i > 2) || (i % 3 == 0 && i > 3))u = 0;
 			if (u)
-				for (j = 5; j * j <= i; j += 2)
+				for (int j = 5; j * j <= i; j += 2)
 					if (i % j == 0)
 					{
 						u = 0;
@@ -36,22 +35,36 @@ class slb
 					}
 			if (u)
 			{
-				zs = j;
+				zs = i;
 				break;
 			}
 		}
 	}
 
+	bool dl();
+
+private:
+	ys a[N];
+	int zs;
+
 	int hx(int x);
+	void ql();
 	int cz(int x);
-	bool cr();
-	bool sc();
-	bool xg();
+	bool zc();
+	bool zx(int x);
+	bool xg(int x);
+	bool xg_new(int x,string y, string z);
 };
 
 int slb::hx(int x)
 {
 	return x % zs;
+}
+
+void slb::ql()
+{
+	cin.clear();
+	cin.ignore();
 }
 
 int slb::cz(int x)
@@ -62,8 +75,177 @@ int slb::cz(int x)
 	return y;
 }
 
+bool slb::zc()
+{
+	int x,x0;
+	string y;
+	bool z = 1;
+	cout << "QQ注册:" << endl;
+	cout << "请输入账号( 3 - 9 位数字):" << endl;
+	getline(cin, y, '\n');
+	for(int i=0;i<y.size();i++)
+		if (y[i] < 48 || y[i]>57)
+		{
+			z = 0;
+			break;
+		}
+	if (z)
+	{
+		x0 = stoi(y);
+		x = cz(x0);
+		a[x].n = x0;
+		cout << "请输入密码:" << endl;
+		getline(cin, y, '\n');
+		a[x].m = y;
+		cout << "请输入昵称:" << endl;
+		getline(cin, y, '\n');
+		a[x].name = y;
+		cout << "\n\n注册成功！" << endl;
+	}
+	else cout << "账号非法！" << endl;
+
+	return z;
+}
+
+bool slb::zx(int x)
+{
+	bool y;
+	cout << "是否注销账号？( 0 - 否 , 1 - 是 )" << endl;
+	cin >> y;
+	ql();
+	if (y)
+	{
+		a[x].n = -1;
+		cout << "\n\n注销成功！" << endl;
+	}
+	else cout << "\n\n注销失败！" << endl;
+
+	return y;
+}
+
+bool slb::xg(int x)
+{
+	string y;
+	int z;
+	cout << "( 0 - 退出修改 , 1 - 修改账号 , 2 - 修改密码 , 3 - 修改昵称  )" << endl;
+	cin >> z;
+	ql();
+	switch (z)
+	{
+	case 0:
+		cout << "\n\n修改失败！" << endl;
+		break;
+	case 1:
+		cout << "请输入新账号:" << endl;
+		cin >> z;
+		ql();
+		a[x].n = -1;
+		xg_new(z, a[x].m, a[x].name);
+		cout << "\n\n修改成功！" << endl;
+		break;
+	case 2:
+		cout << "请输入新密码:" << endl;
+		getline(cin, y, '\n');
+		a[x].m = y;
+		cout << "\n\n修改成功！" << endl;
+		break;
+	case 3:
+		cout << "请输入新昵称:" << endl;
+		getline(cin, y, '\n');
+		a[x].name = y;
+		cout << "\n\n修改成功！" << endl;
+		break;
+	}
+
+	return z;
+}
+
+bool slb::xg_new(int x, string y, string z)
+{
+	int x0 = cz(x);
+	a[x0].n = x;
+	a[x0].m = y;
+	a[x0].name = z;
+
+	return 1;
+}
+
+bool slb::dl()
+{
+	int x;
+	string y;
+	int z = 1;
+	cout << "QQ登录:" << endl;
+	cout << "请输入账号:" << endl;
+	getline(cin, y, '\n');
+	for (int i = 0; i < y.size(); i++)
+		if (y[i] < 48 || y[i]>57)
+		{
+			z = 0;
+			break;
+		}
+	if (z)
+	{
+		x = stoi(y);
+		x = cz(x);
+		if (a[x].n != -1)
+		{
+			cout << "请输入密码:" << endl;
+			getline(cin, y, '\n');
+			if (a[x].m == y)
+			{
+				z = 1;
+				cout << "\n\n登录成功！" << endl;
+				cout << a[x].name << "，欢迎回家！\n\n" << endl;
+			}
+			else cout << "\n密码错误！" << endl;
+		}
+		else
+		{
+			cout << "账号不存在！是否注册账号？( 0 - 否 , 1 - 是 )" << endl;
+			cin >> z;
+			ql();
+			if (z)zc();
+			else 
+				cout << "即将返回" << endl;
+
+			system("pause");
+			system("cls");
+
+			return 0;
+		}
+	}
+	else cout << "账号非法！" << endl;
+
+	if (z)
+	{
+		cout << "操作:  ( 0 - 退出登录 , 1 - 修改账号信息 , 2 - 注销账号 )" << endl;
+		cin >> z;
+		ql();
+		switch (z)
+		{
+		case 0:
+			cout << "即将退出！" << endl;
+			break;
+		case 1:
+			xg(x);
+			break;
+		case 2:
+			zx(x);
+			break;
+		}
+	}
+	system("pause");
+	system("cls");
+
+	return z;
+}
+
 int main()
 {
-
+	int n = 25;
+	slb a;
+	while(n--)
+		a.dl();
 
 }
