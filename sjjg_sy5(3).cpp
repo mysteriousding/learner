db5cbbd495dfe1b547d8b1a9d92bbc5692a47848
kunao_learner <15346777800@163.com>
2025-05-13 22:42:57 +0800

    Add files via upload

---
+++ sjjg_sy5(3).cpp
@@ -0,0 +1,29 @@
+﻿/*
+冒泡排序
+*/
+#include<iostream>
+using namespace std;
+#define N 1000
+
+int main()
+{
+	int n, a[N], j, k, h;
+	cout << "请输入数据个数:" << endl;
+	cin >> n;
+	cout << "请依次输入数据:" << endl;
+	for (int i = 1; i <= n; i++)
+		cin >> a[i];
+	j = n;
+	while (j)
+	{
+		k = j, j = 0;
+		for (int i = 1; i < k; i++)
+			if (a[i] > a[i + 1])
+				h = a[i], a[i] = a[i + 1], a[i + 1] = h, j = i;
+	}
+	cout << "排序结果为:" << endl;
+	for (int i = 1; i <= n; i++)
+		cout << a[i] << " ";
+
+	return 0;
+}
