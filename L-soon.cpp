840d8ecf4bc3d81daf92a2f33ecbc603f169e728
kunao_learner <15346777800@163.com>
2025-06-12 22:57:34 +0800

    Add files via upload

---
+++ L-soon.cpp
@@ -0,0 +1,30 @@
+﻿/*
+6. 加油，2025【算法赛】
+
+问题描述
+时光如梭，转眼间，2025 年即将来临。
+Time flies, and soon, the year 2025 will arrive.
+
+迎接新的一年，我们要以更加积极的态度去探索、去学习。
+As we welcome the new year, let’s adopt a more positive attitude toward exploration and learning.
+
+每一个新的开始都是一次成长的机会，让我们共同努力，实现更大的梦想。
+Every new beginning is an opportunity for growth, so let’s work together to achieve greater dreams.
+
+在这个特别的时刻，请你输出 2025，为你的未来加油！
+At this special moment, please output 2025 to cheer for your future!
+
+输入格式
+本题为填空题，无需输入即可作答。
+
+输出格式
+输出一个数字或字符串，迎接 2025 年。
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << 2025 << endl;
+
+	return 0;
+}
