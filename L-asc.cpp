3b008540470577eade5a07ea227918bb3d15c384
kunao_learner <15346777800@163.com>
2025-06-13 23:14:45 +0800

    Add files via upload

---
+++ L-asc.cpp
@@ -0,0 +1,15 @@
+﻿/*
+ASC
+
+本题为填空题，只需要算出结果后，在代码中使用输出语句将所填结果输出即可。
+
+已知大写字母 A 的 ASCII 码为 65，请问大写字母 L 的 ASCII 码是多少？
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << int('L') << endl;
+
+	return 0;
+}
