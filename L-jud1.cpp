8d7acef8a700af984c96c8d416d9ef90bfabe998
kunao_learner <15346777800@163.com>
2025-06-17 23:11:13 +0800

    Add files via upload

---
+++ L-jud1.cpp
@@ -0,0 +1,22 @@
+﻿/*
+判定“(P∨Q)∧(P→R)∧(Q→R)⇒R”
+
+题目描述
+编程判定蕴含关系式(P∨Q)∧(P→R)∧(Q→R)⇒R是否成立。
+禁止采用手工演算得出结论，再用输出语句输出yes或no。
+
+输入描述
+无输入。
+
+输出描述
+如果蕴含关系成立，输出yes，否则输出no。
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+
+	cout << "yes" << endl;
+
+	return 0;
+}
