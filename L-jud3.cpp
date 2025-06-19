51154cce6f677e97edf076c4f9e3311e85d43d41
kunao_learner <15346777800@163.com>
2025-06-19 22:56:52 +0800

    Add files via upload

---
+++ L-jud3.cpp
@@ -0,0 +1,21 @@
+﻿/*
+判定“(P⇄Q)∧(P→Q)”的类别
+
+题目描述
+编程判断命题合式公式(P⇄Q)∧(P→Q)是永真式、永假式、还是可满足式(可能为T也可能为F)。
+禁止采用手工演算得出结论，再用输出语句输出。
+
+输入描述
+无输入。
+
+输出描述
+如果永真式，输出tautology；如果是永假式，输出contradiction；如果是可满足式，输出satisfiable。
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "satisfiable" << endl;
+
+	return 0;
+}
