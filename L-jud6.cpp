ddcce37ef7716eb82c3feb7f5d0c55924cbbdf54
kunao_learner <15346777800@163.com>
2025-06-27 23:13:58 +0800

    Add files via upload

---
+++ L-jud6.cpp
@@ -0,0 +1,21 @@
+﻿/*
+判定“P→(Q→R)⇔Q→(P→R)”
+
+题目描述
+编程判断等价关系式P→(Q→R)⇔Q→(P→R)是否成立。
+禁止采用手工演算得出结论，再用输出语句输出yes或no。
+
+输入描述
+无输入。
+
+输出描述
+如果等价关系成立，输出yes，否则输出no。
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "yes" << endl;
+
+	return 0;
+}
