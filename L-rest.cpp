626e1e55a15654a56719bb119ac70322973b09d5
kunao_learner <3379584084@qq.com>
2025-08-20 21:27:50 +0800

    Add files via upload

---
+++ L-rest.cpp
@@ -0,0 +1,26 @@
+﻿/*
+约数
+
+题目描述
+本题为填空题，只需要算出结果后，在代码中使用输出语句将所填结果输出即可。
+
+如果整数 a 是整数 b 的整数倍，则称 b 是 a 的约数。
+
+请问，有多少个正整数是 2020 的约数。
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num = 0;
+    for (int i = 1; i * i <= 2020; i++)
+        if (2020 % i == 0)
+        {
+            num += 2;
+            if (i * i == 2020)
+                num--;
+        }
+    cout << num << endl;
+
+    return 0;
+}
