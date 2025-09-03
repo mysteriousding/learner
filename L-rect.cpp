9ab559cc6420727badab68102920be96a3633b85
kunao_learner <3379584084@qq.com>
2025-09-03 22:43:37 +0800

    Add files via upload

---
+++ L-rect.cpp
@@ -0,0 +1,31 @@
+﻿/*
+ 矩形翻转
+
+题目描述
+有一个矩形，长和宽已知，如图(a)所示。现在翻转该矩形90度，如图(b)所示，求翻转后矩形的长和宽。
+
+图片描述
+https://dn-simplecloud.shiyanlou.com/questions/uid1331660-20230926-1695710220313
+
+输入描述
+输入数据占一行，两个正整数a和b，用空格隔开，分别表示矩形的长和宽。
+
+输出描述
+输出占一行，为翻转后矩形的长和宽，用一个空格隔开。
+
+样例输入
+8 5
+
+样例输出
+5 8
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << b << " " << a << endl;
+
+	return 0;
+}
