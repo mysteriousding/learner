098e0483327b9a237c4da72f88dc8a027a0dfea8
kunao_learner <3379584084@qq.com>
2025-09-18 22:34:13 +0800

    Add files via upload

---
+++ LCS1.cpp
@@ -0,0 +1,32 @@
+﻿/*
+LCS1: 暴力递归 计算最长公共子序列的长度
+*/
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+string n, m;
+int u = 0;
+
+// 递归函数，计算 n 的前 a 个字符和 m 的前 b 个字符的 LCS 长度
+int dfs(int a, int b)
+{
+	u++;
+	if (a == 0 || b == 0)
+		return 0;
+	if (n[a - 1] == m[b - 1])
+		return  dfs(a - 1, b - 1) + 1;
+	else
+		return max(dfs(a, b - 1), dfs(a - 1, b));
+}
+
+int main()
+{
+	int num;
+	cin >> n >> m;
+	num = dfs(n.size(), m.size());
+	cout << "LCS Length: " << num << endl;
+	cout << "递归次数: " << u << endl;
+
+	return 0;
+}
