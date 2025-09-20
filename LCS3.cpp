38860ba91c561cc1cd7c47fa10f116482376fb41
kunao_learner <3379584084@qq.com>
2025-09-20 22:03:18 +0800

    Add files via upload

---
+++ LCS3.cpp
@@ -0,0 +1,25 @@
+﻿/*
+LCS3: 动态规划 计算最长公共子序列的长度
+*/
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	string n, m;
+	cin >> n >> m;
+	vector<vector<int>>dp(n.size() + 1, vector<int>(m.size() + 1, 0));
+	for (int i = 1; i <= n.size(); i++)
+		for (int j = 1; j <= m.size(); j++)
+		{
+			if (n[i - 1] == m[j - 1])
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			else
+				dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+		}
+	cout << "LCS Length: " << dp[n.size()][m.size()] << endl;
+
+	return 0;
+}
