78ec051d4ea916e10b4f0c3f2866fca060c975ee
kunao_learner <3379584084@qq.com>
2025-10-26 22:57:38 +0800

    Add files via upload

---
+++ Nsf-1(1).cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int a, b, c, d;
+    string n, m = "", h = "0aA";
+    cin >> a >> b >> c;
+    cin.ignore();
+    getline(cin, n, '\n');
+    for (int i = 0; i < n.size(); i++)
+        if (n[i] == '-' && (n[i + 1] > n[i - 1] && n[i + 1] - n[i - 1] < 26||a==3))
+        {
+            if (a == 3)
+                for (int j = n[i - 1] + 1; j < n[i + 1]; j++)
+                    for (int l = 0; l < b; l++)
+                        m += '*';
+            else
+            {
+                d = n[i + 1] <= '9' ? h[a] : n[i + 1] < 'a' ? 'A' : 'a';
+
+                if (c == 1)
+                    for (int j = n[i - 1] + 1; j < n[i + 1]; j++)
+                        for (int l = 0; l < b; l++)
+                            m += char(j - d + h[a]);
+                else
+                    for (int j = n[i + 1] - 1; j > n[i - 1]; j--)
+                        for (int l = 0; l < b; l++)
+                            m += char(j - d + h[a]);
+            }
+        }
+        else m += n[i];
+    cout << m << endl;
+
+    return 0;
+}
