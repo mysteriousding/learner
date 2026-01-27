26cc19a500b407d995886f0a3e3c500dcc7cc2cb
kunao_learner <3379584084@qq.com>
2026-01-27 22:04:42 +0800

    Add files via upload

---
+++ L-fac1.cpp
@@ -0,0 +1,16 @@
+﻿/*
+约数个数
+
+题目描述
+本题为填空题，只需要算出结果后，在代码中使用输出语句将所填结果输出即可。
+
+1200000 有多少个约数（只计算正约数）。
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << 96 << endl;
+
+	return 0;
+}
