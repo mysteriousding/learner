e4adf82f137b6edcbcb3c570818ccad936aa65c3
kunao_learner <3379584084@qq.com>
2026-06-05 22:56:16 +0800

    Add files via upload

---
+++ L-sho.cpp
@@ -0,0 +1,20 @@
+﻿/*
+最短路
+
+题目描述
+本题为填空题，只需要算出结果后，在代码中使用输出语句将所填结果输出即可。
+
+如下图所示，G 是一个无向图，其中蓝色边的长度是 1、橘色边的长度是 2、绿色边的长度是 3。
+
+https://doc.shiyanlou.com/courses/uid1580206-20210312-1615537376560
+
+则从 A 到 S 的最短距离是多少？
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << 6 << endl;
+
+	return 0;
+}
