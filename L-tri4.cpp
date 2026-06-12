3fc0974f4d23dd4e3738b5cf608cb81b48f998cb
kunao_learner <3379584084@qq.com>
2026-06-12 23:04:22 +0800

    Add files via upload

---
+++ L-tri4.cpp
@@ -0,0 +1,20 @@
+﻿/*
+三角形面积
+
+题目描述
+本题为填空题，只需要算出结果后，在代码中使用输出语句将所填结果输出即可。
+
+https://doc.shiyanlou.com/courses/uid1580206-20210318-1616051049881
+
+如上图所示。图中的所有小方格面积都是 1。
+
+那么，图中的三角形面积应该是多少呢？
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << 28 << endl;
+
+	return 0;
+}
