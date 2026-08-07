84f5dcfcb5cd9930913e8257a04ec7cfb3c41dfa
kunao_learner <3379584084@qq.com>
2026-08-07 22:04:50 +0800

    Add files via upload

---
+++ NK-sjxly7.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    long long n, t, s, u, m = 0;
+    cin >> n;
+    for (long long b = 1; b * b <= n; ++b)
+    {
+        t = 0;
+        while (1)
+        {
+            s = b * b + t * b * (b + 1);
+            if (s > n)
+                break;
+            u = n - s + 1;
+            if (u > b)
+                u = b;
+            m += u;
+            ++t;
+        }
+    }
+    cout << m << endl;
+    return 0;
+}
